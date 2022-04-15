fffb18f77993f4d91b9df7884c9d2b0fca7fa8c5
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2022-04-14 22:03:48 -0400

    all working good

---
+++ backend/lib/server/src/services/database/db.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+
+cluster = MongoClient('mongodb://localhost:27017/')
+db = cluster["cameraia"]
+configs = db["configs"]
+
+def if_exists_return_old_token_otherwise_save_it(token: str) -> str:
+    config_data = configs.find_one({"token": {"$exists": 1}},{"_id": 0,})
+    if config_data: return config_data["token"]
+    configs.insert_one({"token": token})
+    return token
+
+def verify_token(token):
+    config_data = configs.find({"token": token})
+    if config_data: return False
+    return True
