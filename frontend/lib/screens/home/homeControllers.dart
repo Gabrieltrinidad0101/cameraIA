f8d6b362ef3c6568b0862cb0cba721348ec7b904
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2022-05-22 13:41:05 -0400

    my own background services

--- frontend/lib/screens/home/homeControllers.dart
+++ frontend/lib/screens/home/homeControllers.dart
@@ -1,42 +1,34 @@
 import 'package:frontend/constants/socketPathUrl.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:flutter/material.dart' show Navigator;
-import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
-import 'package:flutter/material.dart';
-
-getCamerasVideos() async {
-  print("ok");
-  String url = await getSocketUrl();
-  IO.Socket socket;
-  socket = IO.io(
-      url,
-      IO.OptionBuilder().setTransports(['websocket']) // for Flutter or Dart VM
-          .setExtraHeaders(
-              {'Connection': 'upgrade', 'Upgrade': 'websocket'}) // optional
-          .build());
-  socket.connect();
-  socket.emit("cameras_video", "start");
-  socket.on("camera_detect_obejct", (detectObejct) {
-    socket.emit("message", detectObejct);
-  });
-}
+import 'package:background_services/background_services.dart';
 
 class HomeController {
-  //BackgroundServices backgroundServices = BackgroundServices();
+  BackgroundServices backgroundServices = BackgroundServices();
   HomeController() {
-    initializeBackgroundServices();
-    //backgroundServices.runFunctionInBackground(getCamerasVideos);
+    backgroundServices.runFunctionInBackground(getCamerasVideos);
   }
 
   callDartMethod() {
-    initializeBackgroundServices();
+    backgroundServices.callDartMethod();
   }
 
-  initializeBackgroundServices() async {
-    await AndroidAlarmManager.initialize();
-    final int helloAlarmID = 0;
-    await AndroidAlarmManager.periodic(
-        const Duration(minutes: 2), helloAlarmID, getCamerasVideos);
+  getCamerasVideos() async {
+    print("   start    ");
+    String url = await getSocketUrl();
+    IO.Socket socket;
+    socket = IO.io(
+        url,
+        IO.OptionBuilder()
+            .setTransports(['websocket']) // for Flutter or Dart VM
+            .setExtraHeaders(
+                {'Connection': 'upgrade', 'Upgrade': 'websocket'}) // optional
+            .build());
+    socket.connect();
+    socket.emit("cameras_video", "start");
+    socket.on("camera_detect_obejct", (detectObejct) {
+      socket.emit("message", detectObejct);
+    });
   }
 
   go_to_alarms(context) {
