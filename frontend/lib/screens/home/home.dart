b3794c0eaea02d89d5a09bac12908e86df1e9f67
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2022-06-01 22:17:17 -0400

    create the sign out button, modify log in server change it to when you want to add a new alarm and create button add camera ip

--- frontend/lib/screens/home/home.dart
+++ frontend/lib/screens/home/home.dart
@@ -16,7 +16,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Home"),
+          title: const Text("Home"),
           actions: [
             IconButton(
                 onPressed: () {
